f2fe27d31b89f8b73294757b09025ba830d9b7a9
Pulido1307 <52903296+Pulido1307@users.noreply.github.com>
2020-12-29 23:19:52 -0600

    [Add] Dialogs faltantes

--- app/src/main/java/com/pulido/proyectobd/CrudActivity.java
+++ app/src/main/java/com/pulido/proyectobd/CrudActivity.java
@@ -3,15 +3,19 @@ package com.pulido.proyectobd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -35,24 +39,38 @@ public class CrudActivity extends AppCompatActivity {
         arrayAdapterListView = new ArrayAdapter(CrudActivity.this, android.R.layout.simple_list_item_1, listaImprimir);
         listView_deudas.setAdapter(arrayAdapterListView);
 
+        consultar();
 
+        floatingActionButton_add.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogOpc();
+            }
+        });
 
     }
 
     private void consultar(){
+        listaImprimir.add("Nombre:Anel\nApellidos:Cuevas Duran");
+        listaImprimir.add("Nombre:Antonio\nApellidos:Pulido");
+        listaImprimir.add("Nombre:Vannesa\nApellidos:Cuevas Duran");
     }
-    private void showDialogAddDeudor(){
+
+    public void showDialogOpc(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         LayoutInflater inflater = getLayoutInflater();
 
-        View view =inflater.inflate(R.layout.dialog_add_deudor, null);
-        builder.setView(view).setTitle("Actualizar deudor");
+        View view =inflater.inflate(R.layout.dialog_consultar, null);
+        builder.setView(view).setTitle("Información del cliente");
+
 
         final AlertDialog dialog = builder.create();
 
         dialog.show();
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
